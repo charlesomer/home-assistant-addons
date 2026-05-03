ecdf802d0da34d5d5dd870dd286298442fb38144
Charles Omer <charles@zyndex.co.uk>
2026-05-03 21:02:43 +0100

    test

--- shairport-sync/entrypoint.sh
+++ shairport-sync/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
 
-set -- /run.sh $SUPERVISOR_OPTION_extra_args
+if [ -n "$SUPERVISOR_OPTION_extra_args" ]; then
+    # shellcheck disable=SC2086
+    set -- /run.sh $SUPERVISOR_OPTION_extra_args
+else
+    set -- /run.sh
+fi
+
 exec "$@"
