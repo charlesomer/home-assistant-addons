4a01e5740f832af472f4f5c1535d79070aec237d
Charles Omer <charles@zyndex.co.uk>
2026-05-05 00:44:49 +0100

    test

--- shairport-sync/entrypoint.sh
+++ shairport-sync/entrypoint.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
-set -e
+set -eu
 
-if [ -n "$SUPERVISOR_OPTION_extra_args" ]; then
-    # shellcheck disable=SC2086
-    set -- /run.sh $SUPERVISOR_OPTION_extra_args
-else
-    set -- /run.sh
-fi
+set --
 
-exec "$@"
+# Loop through all SUPERVISOR_OPTION_* variables
+env | while IFS='=' read -r name value; do
+    case "$name" in
+        SUPERVISOR_OPTION_*)
+            key=$(printf "%s" "$name" | sed 's/^SUPERVISOR_OPTION_//')
+
+            # Boolean handling
+            case "$value" in
+                true|1|yes)
+                    set -- "$@" "--$key"
+                    ;;
+                false|0|no|'')
+                    ;;
+                *)
+                    set -- "$@" "--$key" "$value"
+                    ;;
+            esac
+            ;;
+    esac
+done
+
+exec /run.sh "$@"
